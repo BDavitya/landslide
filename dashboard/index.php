10809fd7a8b55f360f35598db10f2d78959e9478
Barita Davitya <bdavitya@gmail.com>
2024-12-19 10:16:01 +0700

    index dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -106,31 +106,89 @@ if(!isset($_SESSION["login"])) {
                 This feature is used to display the number of people who ever lost
                 because of landslide and statistic landslide phenomena in Indonesia.
             </p>
-            <div class="d-flex flex-wrap gap-4 row m-0">
+            <div class="d-flex flex-wrap gap-4 m-0">
                 <div class="d-flex align-items-center py-4 px-5 bg-white shadow rounded col">
                     <div>
-                        <h2 class="d-block text-center">Victims</h2>
+                        <h2 class="d-block text-center">Demage</h2>
                         <div>
-                            <canvas id="VictimsChart"></canvas>
+                            <canvas id="DemageChart"></canvas>
                         </div>
                         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
                         <script>
-                        const ctx = document.getElementById("VictimsChart");
+                        const ctx = document.getElementById("DemageChart");
 
                         new Chart(ctx, {
-                            type: "bar",
+                            type: "pie",
                             data: {
                                 labels: [
-                                    "Red",
-                                    "Blue",
-                                    "Yellow",
-                                    "Green",
-                                    "Purple",
-                                    "Orange",
+                                    "House",
+                                    "Educational Facilities",
+                                    "Health Facilities",
+                                    "Worship Facilities",
+                                    "Public facilities",
+                                    "Office",
+                                    "Bridge",
+                                    "Factory",
+                                    "Stall",
                                 ],
                                 datasets: [{
-                                    label: "# of Votes",
-                                    data: [12, 19, 3, 5, 2, 3],
+                                    label: "Number of Demage",
+                                    data: [
+                                        <?php 
+                                        include ("connect.php");
+                                      $rumah = mysqli_query($koneksi,"select banyak from statistik where label='rumah'");
+                                      $datarumah = mysqli_fetch_array($rumah);
+                                      echo $datarumah['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $fasilitasPendidikan = mysqli_query($koneksi,"select banyak from statistik where label='Fasilitas Pendidikan'");
+                                      $datafasilitasPendidikan = mysqli_fetch_array($fasilitasPendidikan);
+                                      echo $datafasilitasPendidikan['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $fasilitasKesehatan = mysqli_query($koneksi,"select banyak from statistik where label='Fasilitas Kesehatan'");
+                                      $datafasilitasKesehatan = mysqli_fetch_array($fasilitasKesehatan);
+                                      echo $datafasilitasKesehatan['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $fasilitasPeribadahan = mysqli_query($koneksi,"select banyak from statistik where label='Fasilitas Peribadahan'");
+                                      $datafasilitasPeribadahan = mysqli_fetch_array($fasilitasPeribadahan);
+                                      echo $datafasilitasPeribadahan['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $fasilitasUmum = mysqli_query($koneksi,"select banyak from statistik where label='Fasilitas Umum'");
+                                      $datafasilitasUmum = mysqli_fetch_array($fasilitasUmum);
+                                      echo $datafasilitasUmum['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $Perkantoran = mysqli_query($koneksi,"select banyak from statistik where label='Perkantoran'");
+                                      $dataPerkantoran = mysqli_fetch_array($Perkantoran);
+                                      echo $dataPerkantoran['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $Jembatan = mysqli_query($koneksi,"select banyak from statistik where label='Jembatan'");
+                                      $dataJembatan = mysqli_fetch_array($Jembatan);
+                                      echo $dataJembatan['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $Pabrik = mysqli_query($koneksi,"select banyak from statistik where label='Pabrik'");
+                                      $dataPabrik = mysqli_fetch_array($Pabrik);
+                                      echo $dataPabrik['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $Kios = mysqli_query($koneksi,"select banyak from statistik where label='Kios'");
+                                      $dataKios = mysqli_fetch_array($Kios);
+                                      echo $dataKios['banyak'];
+                                      ?>
+                                    ],
                                     borderWidth: 1,
                                 }, ],
                             },
@@ -147,28 +205,58 @@ if(!isset($_SESSION["login"])) {
                 </div>
                 <div class="d-flex align-items-center py-4 px-5 bg-white shadow rounded col">
                     <div>
-                        <h2 class="d-block text-center">Demage</h2>
+                        <h2 class="d-block text-center">Victims</h2>
                         <div>
-                            <canvas id="DemageChart"></canvas>
+                            <canvas id="VictimsChart"></canvas>
                         </div>
                         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
                         <script>
-                        const ctl = document.getElementById("DemageChart");
+                        const ctl = document.getElementById("VictimsChart");
 
                         new Chart(ctl, {
-                            type: "bar",
+                            type: "pie",
                             data: {
                                 labels: [
-                                    "Red",
-                                    "Blue",
-                                    "Yellow",
-                                    "Green",
-                                    "Purple",
-                                    "Orange",
+                                    "Die",
+                                    "Gone",
+                                    "Wounded",
+                                    "Suffer",
+                                    "Flee",
                                 ],
                                 datasets: [{
-                                    label: "# of Votes",
-                                    data: [12, 19, 3, 5, 2, 3],
+                                    label: "Number of Victims",
+                                    data: [
+                                        <?php 
+                                        include ("connect.php");
+                                      $meninggal = mysqli_query($koneksi,"select banyak from statistik where label='Meninggal'");
+                                      $datameninggal = mysqli_fetch_array($meninggal);
+                                      echo $datameninggal['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $hilang = mysqli_query($koneksi,"select banyak from statistik where label='hilang'");
+                                      $datahilang = mysqli_fetch_array($hilang);
+                                      echo $datahilang['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $terluka = mysqli_query($koneksi,"select banyak from statistik where label='terluka'");
+                                      $dataterluka = mysqli_fetch_array($terluka);
+                                      echo $dataterluka['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $menderita = mysqli_query($koneksi,"select banyak from statistik where label='menderita'");
+                                      $datamenderita = mysqli_fetch_array($menderita);
+                                      echo $datamenderita['banyak'];
+                                      ?>,
+                                        <?php 
+                                        include ("connect.php");
+                                      $mengungsi = mysqli_query($koneksi,"select banyak from statistik where label='mengungsi'");
+                                      $datamengungsi = mysqli_fetch_array($mengungsi);
+                                      echo $datamengungsi['banyak'];
+                                      ?>
+                                    ],
                                     borderWidth: 1,
                                 }, ],
                             },
@@ -191,7 +279,7 @@ if(!isset($_SESSION["login"])) {
                     <div>
                         <?php
                         include ("connect.php");
-                        $sql = "SELECT COUNT(*) AS totalData FROM memberships";
+                        $sql = "SELECT COUNT(*) AS totalData FROM oranghilang where status='Masih Dalam Pencarian'";
                         $q1 = mysqli_query($koneksi, $sql);
                         $data = mysqli_fetch_array($q1); 
                         $totalData = $data['totalData']?>
@@ -207,7 +295,7 @@ if(!isset($_SESSION["login"])) {
                     <div>
                         <?php
                         include ("connect.php");
-                        $sql = "SELECT COUNT(*) AS totalData FROM memberships";
+                        $sql = "SELECT COUNT(*) AS totalData FROM oranghilang where status='Sudah Ditemukan'";
                         $q1 = mysqli_query($koneksi, $sql);
                         $data = mysqli_fetch_array($q1); 
                         $totalData = $data['totalData']?>
